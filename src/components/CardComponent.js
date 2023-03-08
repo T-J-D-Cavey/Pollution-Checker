f37eab6d4aeff45b753a507f38f2502f16203cf2
Tim Cavey <caveyt@hotmail.co.uk>
2023-03-08 18:52:52 +0000

    finish cards

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,10 +1,18 @@
 export function CardComponent() {
     return (
         <div>
-            <div>Card 1</div>
-            <div>Card 2</div>
-            <div>Card 3</div>
-            <div>Card 4</div>
+            <a href='https://en.wikipedia.org/wiki/Air_quality_index' target='_blank'>
+                <div>Air Quality Index wiki</div>
+            </a>
+            <a href='https://www.who.int/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health' target='_blank'>
+                <div>World Health Organisation: air pollution</div>
+            </a>
+            <a href='https://waqi.info/' target='_blank'>
+                <div>Where did we source our information?</div>
+            </a>
+            <a href='https://www.actionforcleanair.org.uk/' target='_blank'>
+                <div>What is 'Action for clean air'?</div>
+            </a>
         </div>
     )
 }
