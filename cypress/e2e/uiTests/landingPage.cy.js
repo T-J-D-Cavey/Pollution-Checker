ae5482b74a3c64b446cf3e5bc5fa247131228d2c
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-05-10 22:29:43 +0100

    finish landing page tests

--- cypress/e2e/uiTests/landingPage.cy.js
+++ cypress/e2e/uiTests/landingPage.cy.js
@@ -9,20 +9,35 @@ describe('Landing page', () => {
     it('confirm H1 appears', () => {
     // Gets all elements by tag name:
         cy.get('h1');
-            
+
+    // Selects the only element that Contains the content:
+        cy.get('h1').contains('How clean is the air today?');
+
+    // Selects the only element of that tag that Contains the content
+        cy.contains('h1', 'How clean is the air today?');
+
     // Gets all elements by class name:
         cy.get('.buttonContainer');
+    // Finds the only element that's a descendant of an element with the given className:
+        cy.get('.buttonContainer').find('h1');
         })
 
     it('confirm button appears', () => {
     // Gets all elements by class name:
         cy.get('.mainButton');
     // Gets all elements by tag name: 
-        cy.get('button')
+        cy.get('button');
+    // Gets a button tag that by alt tag:
+        cy.get('[alt="Earth in fumes"]');
+    // Finds the only element that's a descendant of an element with the given className (chained):
+        cy.get('.mainButton').find('button').find('img');
     })
 
     it('confirm footer appears', () => {
     // Gets all elements by specific attributes:
         cy.get("[target='_blank']");
+    // Selects the one element / descendant based on it's content, and tag name:
+        cy.contains('footer', 'See the');
+        cy.contains('footer', "creator's Github page here")
     })
 })
